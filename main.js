e7d29c85da67ab7ab570e45085eebd6a1f6d0d7e
Leonardo Balestrin Depelegrin <leobd2506@gmail.com>
2024-12-11 12:13:55 -0300

    adicionando front v1

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 //import Mesa from './ObjectMesa'
-const Mesa = require('./ObjectMesa');
+//const Mesa = require('./ObjectMesa');
 
 function rollD6(){
     return Math.round(Math.random()*5)+1;
 }
 
+function catchPosition(){
+
+}
+
+addEventListener('click',function(event){
+    let idObject = event.target.id;
+})
